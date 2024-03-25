b948eaa034c2012a0bc74df666cdc6fa5c7dfe9a
devgoncalo <goncalopintoneto@gmail.com>
2024-03-25 15:13:27 +0000

    fix: New Chat Creation

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,8 +1,21 @@
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+
 import ChatInput from "@/components/chat/chat-input";
 import NewChat from "@/components/chat/new-chat";
 import MobileMenuButton from "@/components/navigation/mobile-menu-button";
+import useChats from "@/hooks/useChats";
 
 export default function Home() {
+  const pathname = usePathname();
+  const { addChatHandler } = useChats();
+  const [chatCreated, setChatCreated] = useState(false);
+
+  if (!chatCreated && pathname === "/chat") {
+    addChatHandler();
+    setChatCreated(true);
+  }
+  
   return (
     <main className="relative flex flex-col items-stretch flex-1 w-full h-full ml-0 overflow-hidden transition-all transition-width md:ml-64 dark:bg-neutral-900 bg-neutral-50">
       <div className="flex-1 overflow-hidden">
